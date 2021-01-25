e81aeaf67d245db2ec3aa043beafcbe3ba7c7ce3
ideven85 <63672944+ideven85@users.noreply.github.com>
2021-01-25 07:27:35 +0530

    Add files via upload

---
+++ src/test/java/com/cleo/amazon/AmazonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleo.amazon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
